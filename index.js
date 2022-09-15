0b681c753b2101ed1065101643b044ace41f07b5
Christabel Alorzuke <alorzuke2000@gmail.com>
2022-09-15 16:59:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {dbConnect} = require("./config/dbConnect");
+const productsRouter = require("./modules/products/products.route");
+const authRouter = require("./modules/users/auth.routes")
+
+const app = express();
+app.use(express.json())
+
+// app.get("/", (req, res) => {
+//     res.status(200).send("welcome to my server");
+// });
+
+app.use("/products", productsRouter);
+app.use("/auth", authRouter);
+
+
+async function start() {
+    await dbConnect();
+    app.listen(4000, (req, res) =>{
+       console.log("server runing on https://localhost/4000")
+    });
+}
+
+
+start();
